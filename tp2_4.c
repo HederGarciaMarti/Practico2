62831199196fcf42249410675c075c3e5ca1af4d
Heder <hedersafe.2016@gmail.com>
2025-04-13 16:21:57 -0300

    Agregue cargarComputadora y listarPCs

--- tp2_4.c
+++ tp2_4.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <funciones.h>
+#include "funciones.h"
 
 int main (){
     compu computadoras[5];
-    
+    cargarComputadora(computadoras, 5);
+    listarPCs(computadoras, 5);
 }
